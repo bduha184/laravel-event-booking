a78ad616ccd9c531175129109393ee101db626f8
bduha184 <bduha_184@yahoo.co.jp>
2023-06-06 21:50:10 +0900

    livewire test

---
+++ app/Http/Livewire/Register.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Livewire\Component;
+
+class Register extends Component
+{
+
+    public $name;
+    public $email;
+    public $password;
+
+    protected $rules = [
+        'name'=>'required|string|max:255',
+        'email'=>'required|string|email|max:255|unique:users',
+        'password'=>'required|string|min:8'
+    ];
+
+    public function render()
+    {
+        return view('livewire.register');
+    }
+
+    public function updated($validate){
+        $this->validateOnly($validate);
+    }
+    public function register(){
+        $this->validate();
+        User::create([
+            'name'=>$this->name,
+            'email'=>$this->email,
+            'password'=>Hash::make($this->password)
+        ]);
+
+    session()->flash('message','登録完了しました');
+
+    return to_route('livewire-test.index');
+    }
+
+
+}
