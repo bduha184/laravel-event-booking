7b50c96e2d7490accd2557e5784cdbf083cb7cfb
bduha184 <bduha_184@yahoo.co.jp>
2023-06-06 22:36:21 +0900

    logo

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,5 +1,7 @@
-<div class="p-6 lg:p-8 bg-white border-b border-gray-200">
-    <x-application-logo class="block h-12 w-auto" />
+<div class=" p-6 lg:p-8 bg-white border-b border-gray-200">
+    <div class="w-20">
+        <x-application-logo class="block w-auto" />
+    </div>
 
     <h1 class="mt-8 text-2xl font-medium text-gray-900">
         Welcome to your Jetstream application!
