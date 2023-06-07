480fb023ac1edb96987844d51db8b222faa626b9
bduha184 <bduha_184@yahoo.co.jp>
2023-06-07 14:17:34 +0900

    add gate

--- routes/web.php
+++ routes/web.php
@@ -30,12 +30,27 @@ Route::middleware([
 });
 
 Route::controller(LivewireTestController::class)
-->prefix('livewire-test')
-->name('livewire-test.')
-->group(function()
-{
-    Route::get('index','index')->name('index');
-    Route::get('register','register')->name('register');
-});
+    ->prefix('livewire-test')
+    ->name('livewire-test.')
+    ->group(function () {
+        Route::get('index', 'index')->name('index');
+        Route::get('register', 'register')->name('register');
+    });
 
-Route::get('alpine-test/index',[AlpineTestController::class,'index']);
+Route::get('alpine-test/index', [AlpineTestController::class, 'index']);
+
+
+Route::prefix('manager')
+    ->middleware('can:manager-higher')
+    ->group(function () {
+        Route::get('index', function () {
+            dd('manager');
+        });
+    });
+
+Route::middleware('can:user-higher')
+    ->group(function () {
+        Route::get('index', function () {
+            dd('manager');
+        });
+    });
